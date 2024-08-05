c5bd77d699ca9377d8081e08ea3f8c11ad105a64
DeLuthpi <dedeluthpi006@gmail.com>
2024-08-06 00:38:32 +0700

    styling page not found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -11,23 +11,26 @@ const NotFound = (props) => {
 
 	return (
 		<div className="error-page">
-			<div className="container">
-				<Link className="navbar-brand font-weight-bolder ms-lg-0 ms-3 " to="/">
-					<img src={props?.logo} className="navbar-brand-img h-100" alt="logo-image" />
-					{props?.webName}
-				</Link>
+			<div className="error-header">
+				<div className="container">
+					<Link className="error-logo " to="/">
+						<img src={props?.logo} className="navbar-brand-img h-100" alt="logo-image" />
+						{props?.webName}
+					</Link>
+				</div>
 			</div>
-		
-			<div className="container">
-				<div className="row">
-					<div className="col-lg-6 my-auto">
-						<h1 className="display-1 text-bolder text-gradient text-danger">Error 404</h1>
-						<h2>Erm. Page not found</h2>
-						<p className="lead">We suggest you to go to the homepage while we solve this issue.</p>
-						<button type="button" onClick={handleClick} className="btn bg-gradient-dark mt-4">Go to Homepage</button>
-					</div>
-					<div className="col-lg-6 my-auto position-relative">
-						<img className="w-100 position-relative" src={props?.error404} alt="error-404" />
+			<div className="error-section">
+				<div className="container">
+					<div className="row">
+						<div className="col-text">
+							<h1 className="error-h1">Error 404</h1>
+							<h2>Erm. Page not found</h2>
+							<p className="lead">We suggest you to go to the homepage while we solve this issue.</p>
+							<button type="button" onClick={handleClick} className="btn btn-to-homepage">Go to Homepage</button>
+						</div>
+						<div className="col-image">
+							<img className="error-image" src={props?.error404} alt="error-404" />
+						</div>
 					</div>
 				</div>
 			</div>
