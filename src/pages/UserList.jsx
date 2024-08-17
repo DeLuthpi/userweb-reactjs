d8b17732451382eaa29e23dd7a3e5f74d579292e
DeLuthpi <dedeluthpi006@gmail.com>
2024-08-17 23:27:08 +0700

    fix home and userlist page view

--- src/pages/UserList.jsx
+++ src/pages/UserList.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
 import '../assets/css/userlist-style.css';
 import Navbar from '../components/Navbar';
@@ -5,16 +6,33 @@ import SideNavbar from '../components/SideNav';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { RingLoader } from 'react-spinners';
+import { Link } from "react-router-dom";
 
 const UserList = () => {
 	const [users, setUsers] = useState([]);
+	const [pagination, setPagination] = useState({
+		currentPage: 1,
+		nextPage: 0,
+		prevPage: 0,
+		showFrom: 1,
+		showTo: 6,
+		totalUsers: 0,
+	});
 
 	const getUsers = () => {
 		axios
-		.get('https://reqres.in/api/users')
+		.get(`https://reqres.in/api/users?per_page=6&page=${pagination.currentPage}`)
 		.then((res) => {
-			// console.log(res.data.data);
+			// console.log(res.data);
 			setUsers(res.data.data);
+			setPagination({
+				currentPage: res.data.page,
+				nextPage: res.data.page === res.data.total_pages ? 0 : res.data.page + 1,
+				prevPage: res.data.page === 1 ? 0 : res.data.page - 1,
+				showFrom: res.data.page * 6 - 5,
+				showTo: res.data.page * 6,
+				totalUsers: res.data.total,
+			})
 		})
 		.catch((err) => {
 			console.log(err.response)
@@ -23,7 +41,20 @@ const UserList = () => {
 
 	useEffect(() => {
 		getUsers()
-	}, []);
+	}, [pagination.currentPage]);
+
+	const handleNext = () => {
+		setPagination({
+			...pagination,
+			currentPage: pagination.currentPage + 1,
+		});
+	};
+	const handlePrevious = () => {
+		setPagination({
+		...pagination,
+		currentPage: pagination.currentPage - 1,
+		});
+	};
 
 	return (
 		<div className="main-page sidenav-show " id="main-page">
@@ -41,17 +72,28 @@ const UserList = () => {
 							<div className="ulsr-row-flex">
 								{users.map(user => (
 									<div className="card rflex-card" key={user.id}>
-										<div className="card-body">
-											<div className="cb-rflex">
-												<img className="rflex-cbimage" src={user.avatar} alt="user-image" />
-												<div className="rflex-col">
-													<h5 className="rflex-cbtitle">{user.first_name} {user.last_name}</h5>
-													<p className="rflex-cbtext">{user.email}</p>
+										<Link to={`/user-list/user-detail/${user.id}`}>
+											<div className="card-body rflex-cb">
+												<div className="cb-rflex">
+													<img className="rflex-cbimage" src={user.avatar} alt="user-image" />
+													<div className="rflex-col">
+														<h5 className="rflex-cbtitle">{user.first_name} {user.last_name}</h5>
+														<p className="rflex-cbtext">{user.email}</p>
+													</div>
 												</div>
 											</div>
-										</div>
+										</Link>
 									</div>
 								))}
+							</div>
+							<div className="ulsr-footer-data">
+								<div className="uslr-data-info">
+									Showing {pagination.showFrom} to {pagination.showTo} of {pagination.totalUsers} user&apos;s
+								</div>
+								<div className="ulsr-data-pagination">
+									<button className="btn-pagination" onClick={handlePrevious} disabled={pagination.prevPage === 0}>back</button>
+									<button className="btn-pagination" onClick={handleNext} disabled={pagination.nextPage === 0}>next</button>
+								</div>
 							</div>
 						</div>
 					</div>
